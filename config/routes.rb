d04bf27ef03e9a828090b58191a0712efe2edd6e
Gorka Echavarri <gorkaechavarri@gmail.com>
2023-09-07 18:13:24 +0200

    numbers formatted correctly

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,5 @@ Rails.application.routes.draw do
   # root to: 'pages#home'
   devise_for :users, controllers: { registrations: 'registrations' }
 
-  # get 'landing_page', to: 'landing#sign_in'
 
 end
