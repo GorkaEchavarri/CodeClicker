ff5b36fe6416c658f07b3f811ca496bfffb72efc
Gorka Echavarri <gorkaechavarri@gmail.com>
2023-10-25 11:48:36 +0100

    devise and user authenticaiton removed

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,8 @@
 Rails.application.routes.draw do
-  devise_scope :user do
-    authenticated :user do
-      root 'pages#home', as: :authenticated_root
-    end
 
-    unauthenticated do
-      root 'devise/sessions#new', as: :unauthenticated_root
-    end
-  end
 
-  # root to: 'pages#home'
-  devise_for :users, controllers: { registrations: 'registrations' }
+
+  root to: 'pages#home'
 
   # get 'landing_page', to: 'landing#sign_in'
 
